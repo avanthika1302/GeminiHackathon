dbe545d7428a175808a7c576b7de83abee668e41
AVANTHIKA SANKARARAMAN <s.avanthika@gmail.com>
2025-04-23 20:41:52 -0700

    Created using Colab

--- photo_assistant_chatbot.py
+++ photo_assistant_chatbot.py
@@ -23,7 +23,7 @@
         "colab_type": "text"
       },
       "source": [
-        "<a href=\"https://colab.research.google.com/github/avanthika1302/GeminiHackathon/blob/main/photo_assistant_chatbot.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+        "<a href=\"https://colab.research.google.com/github/avanthika1302/Photo-Assistant-Chatbot/blob/main/photo_assistant_chatbot.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
       ]
     },
     {
